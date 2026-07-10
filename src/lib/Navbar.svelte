2a71bd075e93ba53947782e2a08398a6aa01a5e3
adamkasten <adam.kasten@outlook.com>
2026-07-10 17:49:29 -0500

    navbar updates with scrollto button for desktop and mobile

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,7 +1,18 @@
 <script>
+	import { smootherstep } from "three/src/math/MathUtils.js";
+  import { page } from '$app/state';
+
 
   function hover_animation() {
 
+  }
+
+  function scroll_to_content() {
+    const content = document.getElementById("home_content");
+    content?.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+    });
   }
 
 </script>
@@ -12,6 +23,9 @@
     <a href="/projects" class="nav_button space_grotesk_norm"><li class="crumb">projects</li></a>
     <a href="/contact" class="nav_button space_grotesk_norm"><li class="crumb">contact</li></a>
   </ol>
+  {#if page.url.pathname === '/'}
+  <button type="button" id="scroll_btn" onclick={scroll_to_content}>&#8623;</button>
+  {/if}
 </nav>
 
 <style>
@@ -20,12 +34,14 @@
     color: var(--creamy);
     width: 200px;
     flex-shrink: 0;
+    z-index: 99;
   }
   .crumbs ol {
     list-style-type: none;
     padding-left: 0;
     position: sticky;
     top: 0;
+    z-index: 2;
   }
 
   .nav_button {
@@ -45,6 +61,7 @@
     font-weight: bold;
     transition: all .3s ease-in-out;
     background: rgba(39, 1, 65, 0.014);
+    
   }
   .crumb {
     list-style-type: none;
@@ -56,6 +73,28 @@
     background-color: var(--indigo);
     transition: all .3s ease-in-out;
   }
+
+  #scroll_btn {
+    display: inline-block;
+    width: 100%;
+    height: auto;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    font-size: 2rem;
+    padding: 10px 30px;
+    cursor: pointer;
+    text-align: center;
+    text-decoration: none;
+    line-height: 1;
+    text-overflow: ellipsis;
+    border-style: none;
+    font-weight: bold;
+    transition: all .3s ease-in-out;
+    background: rgba(39, 1, 65, 0.014);
+    background: var(--accent);
+    position: relative;
+    z-index: 0;
+}
 
   	@media (width <= 1300px) {
       nav {
